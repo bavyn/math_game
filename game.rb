63608e0f39162068b7c91f8dce55f626f2cdc8c3
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-06-29 14:15:16 -0700

    pseudo code Game class and public methods

---
+++ game.rb
@@ -0,0 +1,35 @@
+class Game
+  def initialize(p1, p2)
+    @p1 = p1
+    @p2 = p2
+    @current_player = p1 #player 1 will have the first turn
+  end
+
+  def start_game
+    # define turn as a new Turn with current player and new question
+    # if the game is over, break
+    # switch turn
+    #end game
+  end
+
+  def switch_turn
+    # ternerary operator. check who current player is and set the true to other player
+  end
+
+  def game_over?
+    # true if either player has 0 lives
+  end
+
+  def end_game
+    # define winner as the person who doesnt have 0 lives
+    # define loser as the person who does have 0 lives
+
+    puts "----- GAME OVER -----"
+    puts "#{winner.name} wins with a score of #{winner.score}"
+    puts "Good bye!"
+
+  end
+
+
+
+end
