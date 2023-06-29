d148ca267dec4ee5bf8cd082bd85952f14781e88
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-06-29 15:03:14 -0700

    add question

--- question.rb
+++ question.rb
@@ -3,6 +3,7 @@ class Question
     @number_a = rand(1..20)
     @number_b = rand(1..20)
     @answer = @number_a + @number_b
+    puts "What is #{@number_a} + #{@number_b}?"
   end
 
   def check_answer
